53befded264b8542488a2e15a3018cf7c8b521e8
ApertureSecurity <tatrasiel7@gmail.com>
2017-11-25 15:40:53 +0200

    added some dots

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ window.onload = function(){
                 td = document.createElement('td');
                 tr.appendChild(td);
                 span = document.createElement('span');
-                span.innerHTML = ".";
+                span.innerHTML = "&#9679";
                 td.appendChild(span);
                 rect = span.getBoundingClientRect();
                 span.id = rect.x + " " + rect.y;
@@ -35,28 +35,26 @@ window.onload = function(){
         i--;
     }
 
-  /*  document.getElementById("main_table").onclick = function(event) {
-        var x = event.clientX;
-        var y = event.clientY;
-        var coords = "X coords: " + x + ", Y coords: " + y;
-        document.getElementById("demo").innerHTML = coords;
-    }*/
 }
 
 var previousLocation = [{}]
 var LocationOfTochka =  {}
 
 
-function DrawLine(event){
-  var x = event.clientX;
-  var y = event.clientY;
-//  console.log(event);
-  previousLocation.push({"x":x ,"y":y})
 
 
+function DrawLine(){
 
-  LocationOfTochka = ({"x":x, "y":y})
-  console.log(LocationOfTochka.x)
+var x = document.getElementById("x").value
+var y = document.getElementById("y").value
+
+LocationOfTochka = ({"x":x, "y":y})
+console.log(LocationOfTochka)
+
+
+var kuda_x = document.getElementById("kuda_x").value
+var kuda_y = document.getElementById("kuda_y").value
+
 
 
 var c=document.getElementById("internalcanvas");
@@ -64,16 +62,42 @@ var ctx=c.getContext("2d");
 ctx.beginPath();
 var secondtolastitem =  ([previousLocation.length-2])
 //console.log(previousLocation[secondtolastitem])
-if (secondtolastitem == null){console.log("One Item in Array")}
+if (LocationOfTochka == null){console.log("One Item in Array")}
 
-else {
-//  console.log(previousLocation[secondtolastitem].x)
-//ctx.moveTo(previousLocation[secondtolastitem].x,previousLocation[secondtolastitem].y);
+  else {
+  //  console.log(previousLocation[secondtolastitem].x)
+  //ctx.moveTo(previousLocation[secondtolastitem].x,previousLocation[secondtolastitem].y);
+  ctx.moveTo(LocationOfTochka.x,LocationOfTochka.y)
+  ctx.lineTo(kuda_x,kuda_y);
+  ctx.stroke();
+  }
+};
 
-ctx.moveTo(LocationOfTochka.x,LocationOfTochka.y)
-ctx.lineTo(100,100);
-ctx.stroke();
+
+
+var PenLocation = {"x":0, "y":0};
+
+function SetPen(){
+
+
+
+  var x = document.getElementById("x").value
+  var y = document.getElementById("y").value
+
+if(x && y){
+  PenLocation = ({"x":x, "y":y})
+  }
 }
+
+
+function PickupPen(){
+PenLocation = ({"x":null, "y":null})
+
+}
+
+
+function moveToVector(){
+
 
 
 
